f43c27e6fc3a874e4d3a907a8c7ae8a09311ccb0
codemobiles <chaiyasit.t@gmail.com>
2021-04-24 17:56:19 +0700

    cmstock_fast_api_ep6 - intro pydantic and register

--- app/api/authen.py
+++ app/api/authen.py
@@ -1,8 +1,21 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from typing import Optional
 
 router = APIRouter()
+
+
+class User(BaseModel):
+    username: str
+    password: str
+    level: Optional[str] = "normal"
 
 
 @router.get("/authen")
 def get_authen():
     return {"result": "authen"}
+
+
+@router.post("/register")
+def register(user: User):
+    return user
