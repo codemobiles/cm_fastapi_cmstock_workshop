60d4923aa250e88bd4af84fc067c44447a76e863
codemobiles <chaiyasit.t@gmail.com>
2021-04-25 17:41:47 +0700

    cmstock_fast_api_ep17 - Add upload image field

--- app/api/product.py
+++ app/api/product.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Form, Depends
+from fastapi import APIRouter, Form, Depends, UploadFile, File
 from pydantic import BaseModel
 from typing import Optional
 from app.api import schema
@@ -7,7 +7,8 @@ from app.db import get_db
 from sqlalchemy.orm import Session
 from app.models.Product import Product as ProductDB
 from app.api import security
-
+from pathlib import Path
+import os
 
 router = APIRouter()
 
@@ -26,12 +27,13 @@ def get_product_form(id: Optional[str] = Form(None),
 
 @router.post("/")
 async def insert_product(product: schema.Product = Depends(get_product_form),
+                         image: UploadFile = File(...),
                          db: Session = Depends(get_db)):
     try:
         db_product = ProductDB(**product.dict())
         db.add(db_product)
         db.commit()
-        return {"result": "ok"}
+        return {"result": "ok", "image": image}
     except Exception as e:
         return {"product": "nok", "error": str(e)}
 
