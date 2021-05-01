e1a56dd7cec36451ae26c8c417a18ac5333c3f94
codemobiles <chaiyasit.t@gmail.com>
2021-05-01 16:53:17 +0700

    cmstock_fast_api_ep35 - validator in pydantic

--- app/config/setting.py
+++ app/config/setting.py
@@ -9,6 +9,14 @@ class Settings(BaseSettings):
         "http://localhost",
         "http://localhost:8085"]
 
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+
     PROJECT_NAME: str = "CMSTOCK"
 
     class Config:
